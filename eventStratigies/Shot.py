abfb3cf9676d0b8b79b9a2c905e7d31d44b16c7b
Rayyan Alam <rayyan@moralis.io>
2024-03-31 16:13:16 -0400

    feat: add start pattern

---
+++ eventStratigies/Shot.py
@@ -0,0 +1,8 @@
+
+
+class Shot:
+    id = 10
+    def __init__(self):
+        pass
+    def handle(self,payload):
+        print("Inserting Shot  " + payload)
---
+++ eventStratigies/test.py
@@ -0,0 +1,8 @@
+from Shot import Shot
+from EventStrategyManager import EventStrategyManager
+
+if __name__ == '__main__':
+    context = EventStrategyManager()
+    x= context.get_strategy_by_id(10)
+    x.handle("Testing")
+    print("Testing")
