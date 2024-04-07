451533780c64a6bf2c218970ccbd94aa734b744e
Rayyan Alam <rayyan@moralis.io>
2024-04-07 06:47:33 -0400

    feat: q2

--- queries.py
+++ queries.py
@@ -40,5 +40,18 @@ def q_1():
     #         print(results[i], "Not found in csv")
     return results
 
+def q_2():
+    seasonId = 4
+    query = """
+    SELECT player_name, COUNT(SEM.event_id)
+      FROM season_event_mapping AS SEM
+      JOIN event_shot ON SEM.event_id = event_shot.event_id
+      JOIN player ON player.player_id = event_shot.event_player_id
+      WHERE SEM.season_id = %s and event_shot.event_statsbomb_xg > 0
+      GROUP BY player_name
+          """
+    cursor.execute(query, (seasonId,))
+    results = cursor.fetchall()
+
 if __name__ == '__main__':
     q_1()
