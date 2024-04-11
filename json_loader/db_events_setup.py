eccb3c9fc0592335bfa3a59e0faae18de1adfbf4
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2024-04-11 16:22:28 -0400

    added data folder

--- json_loader/db_events_setup.py
+++ json_loader/db_events_setup.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
 
     matchesInQuery = getRequriedMatchIds()
     matchMetadataMapping = loadMatchMetadataMapping(matchesInQuery, cursor)
-    eventFolder = os.environ.get("EVENT_FOLDER_PATH", os.path.join("data","events"))
+    eventFolder = os.environ.get("EVENT_FOLDER_PATH", os.path.join("data","eventsSmall"))
 
     for file in os.listdir(eventFolder):
         matchId = int(file.split('.')[0])
